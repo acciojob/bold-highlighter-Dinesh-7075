075c39cfd4797ed622856bb21e09189ffe98e414
Dinesh_Reddy <119753519+Dinesh-7075@users.noreply.github.com>
2023-06-18 16:41:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function highlight() {
     //Write your code here
-
-
+        const collection = document.getElementsByTagName('strong');
+        collection.style.color = "green";
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+   const collection = document.getElementsByTagName('strong');
+    collection.style.color = "black";
 }
